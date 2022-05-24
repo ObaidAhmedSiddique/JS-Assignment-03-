b19422f568f406a43511b1360a1d5b93f1e77654
ObaidAhmedSiddique <iteb4.obaid@gmail.com>
2022-05-24 04:38:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// ||||||||||||||||||||||||||||||||||||| MATHS EXPRESSIONS (Ch # 6 - 9) |||||||||||||||||||||||||||||||||||||||||||
+
+// ***************************************** Question # 01 *****************************************
+
+// var a = +prompt("Enter Your Number:");
+// document.write("Result: <br>");
+// document.write("The value of a is:" + a + "<br>");
+// document.write("................................................ <br> <br>");
+// document.write("The value of ++a is:" + ++a + "<br>");
+// document.write("Now the value of a is:" + a + "<br> <br>");
+// document.write("The value of a++ is:" + a++ + "<br>");
+// document.write("Now the value of a is:" + a + "<br> <br>");
+// document.write("The value of --a is:" + --a + "<br>");
+// document.write("Now the value of a is:" + a + "<br> <br>");
+// document.write("The value of a-- is:" + a-- + "<br>");
+// document.write("Now the value of a is:" + a + "<br>");
+
+// ***************************************** Question # 02 *****************************************
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("--a; (The value of --a is: 1) <br>")
+// document.write("--a - --b; (The value of --b is 0 So, 1 - 0) <br>")
+// document.write("--a - --b + ++b; (The value of ++b is 1 So, 1 - 0 - 1) <br>")
+// document.write("--a - --b + ++b + b--; (The value of b-- is 1 So, 1 - 0 + 1 + 1)  <br> <br>")
+// document.write("a is " + a + "<br>");
+// document.write("b is " + b + "<br>");
+// document.write("result is " + result);
+
+// ***************************************** Question # 03 *****************************************
+
+// var name = prompt("Enter Your Name:");
+// alert("Congratulations!")
+
+// ***************************************** Question # 04 *****************************************
+
+// var num = prompt(" Enter a Number:")
+// document.write("Multiplication table of " + num + "<br>");
+// if(num === num){
+//     document.write(num + "x 1 = " + num + "<br>")
+//     document.write(num + "x 2 = " + num*2 + "<br>")
+//     document.write(num + "x 3 = " + num*3 + "<br>")
+//     document.write(num + "x 4 = " + num*4 + "<br>")
+//     document.write(num + "x 5 = " + num*5 + "<br>")
+//     document.write(num + "x 6 = " + num*6 + "<br>")
+//     document.write(num + "x 7 = " + num*7 + "<br>")
+//     document.write(num + "x 8 = " + num*8 + "<br>")
+//     document.write(num + "x 9 = " + num*9 + "<br>")
+//     document.write(num + "x 10 = " + num*10 + "<br>")
+// }
+// else{
+//     document.write("Multiplication table of 5" + "<br>");
+//     document.write("5 x 1 = 5"+ "<br>")
+//     document.write("5 x 2 = 10"+ "<br>")
+//     document.write("5 x 3 = 15"+ "<br>")
+//     document.write("5 x 4 = 20"+ "<br>")
+//     document.write("5 x 5 = 25"+ "<br>")
+//     document.write("5 x 6 = 30"+ "<br>")
+//     document.write("5 x 7 = 35"+ "<br>")
+//     document.write("5 x 8 = 40"+ "<br>")
+//     document.write("5 x 9 = 45"+ "<br>")
+//     document.write("5 x 10 = 50"+ "<br>")
+// }
